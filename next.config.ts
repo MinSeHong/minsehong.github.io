fee9b9a410b2b8161f701d59afbc449377cac10b
MinSeHong <rebellia@naver.com>
2025-05-16 10:30:10 +0900

    "Next JS 홈페이지 배포"

---
+++ next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+    typescript: {
+    // 빌드 중에 타입스크립트 오류를 무시하도록 설정
+    ignoreBuildErrors: true,
+    },
+    eslint: {
+      ignoreDuringBuilds: true, // 빌드 중 ESLint 오류를 무시
+    },
+};
+
+export default nextConfig;
